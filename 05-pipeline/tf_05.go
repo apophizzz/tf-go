a3155e75f939d5ae3791e04cb05cad11330f8385
patrick.kleindienst <patrick.kleindienst@web.de>
2017-01-09 22:17:42 +0100

    Started with first two parts of pipeline style: 1) Reading input file. 2) Using regex to replace all non-alphanumeric characters by whitespace.

---
+++ 05-pipeline/tf_05.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"io/ioutil"
+	"regexp"
+	"strings"
+	"log"
+)
+
+func readFile(filePath string) string {
+	fileData, err := ioutil.ReadFile(filePath)
+
+	if(err != nil) {
+		panic(err)
+	}
+
+	return string(fileData)
+}
+
+
+func filterCharsAndNormalize(data string) string {
+	regexp, err := regexp.Compile("[\\W_]+")
+
+	if(err != nil) {
+		panic(err)
+	}
+
+	return strings.ToLower(regexp.ReplaceAllLiteralString(data, " "))
+}
+
+
+func main() {
+	log.Print(filterCharsAndNormalize(readFile("input.txt")))
+}
