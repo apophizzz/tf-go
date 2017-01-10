60951e6b90e583d035382ddff9b6e401891a6412
patrick.kleindienst <patrick.kleindienst@web.de>
2017-01-10 21:35:50 +0100

    Implemented method for checking if a key exists in a list of sortable pairs; Added method for fetching a pair which matches a certain key.

--- util/types.go
+++ util/types.go
@@ -1,5 +1,10 @@
 package util
 
+import (
+	"fmt"
+	"errors"
+)
+
 type SortablePair struct {
 	Key string
 	Val int
@@ -20,4 +25,23 @@ func (pairList SortablePairList) Less(i, j int) bool {
 
 func (pairList SortablePairList) Swap(i, j int) {
 	pairList[i], pairList[j] = pairList[j], pairList[i]
+}
+
+func (pairList SortablePairList) ContainsKey(key string) bool {
+	for _, pair := range pairList {
+		if (key == pair.Key) {
+			return true
+		}
+	}
+	return false
+}
+
+func (pairList SortablePairList) Get(key string) (*SortablePair, error) {
+	for _, pair := range pairList {
+		if (key == pair.Key) {
+			return pair, nil
+		}
+	}
+
+	return nil, errors.New(fmt.Sprintf("No key matching %s.", key))
 }
