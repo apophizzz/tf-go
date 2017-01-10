a934fd0ad6a6788cf5a18c61ad86c00b9c78fcb0
patrick.kleindienst <patrick.kleindienst@web.de>
2017-01-10 20:51:35 +0100

    Code formatting.

--- 05-pipeline/tf_05.go
+++ 05-pipeline/tf_05.go
@@ -10,24 +10,22 @@ import (
 func readFile(filePath string) string {
 	fileData, err := ioutil.ReadFile(filePath)
 
-	if(err != nil) {
+	if (err != nil) {
 		panic(err)
 	}
 
 	return string(fileData)
 }
 
-
 func filterCharsAndNormalize(data string) string {
 	regexp, err := regexp.Compile("[\\W_]+")
 
-	if(err != nil) {
+	if (err != nil) {
 		panic(err)
 	}
 
 	return strings.ToLower(regexp.ReplaceAllLiteralString(data, " "))
 }
-
 
 func main() {
 	log.Print(filterCharsAndNormalize(readFile("input.txt")))
