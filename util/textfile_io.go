a256bc89020cc0ef4b2415b90ac56ab0d6ff5b13
patrick.kleindienst <patrick.kleindienst@web.de>
2017-03-01 17:10:10 +0100

    Improved documentation.

--- util/textfile_io.go
+++ util/textfile_io.go
@@ -3,8 +3,8 @@ package util
 import "io/ioutil"
 
 /*
-	Takes a path to a text file as argument and returns its contents as a single string.
-	If no file could be found under the given path, an error is thrown.
+	Takes a path to a text file as an argument and returns the file's contents
+	as a single string. If no file could be found under the given path, an error is thrown.
  */
 func ContentAsString(filePath string) string {
 	data, err := ioutil.ReadFile(filePath)
