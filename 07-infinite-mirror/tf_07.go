3ad750015a5df2738ecc778f1258f9941d198b62
patrick.kleindienst <patrick.kleindienst@web.de>
2017-03-03 18:39:01 +0100

    Implemented functions for reading words and stop words.

--- 07-infinite-mirror/tf_07.go
+++ 07-infinite-mirror/tf_07.go
@@ -1,14 +1,42 @@
 package main
 
-import "github.com/PaddySmalls/golang_term-frequency-styles/util"
+import (
+	"github.com/PaddySmalls/golang_term-frequency-styles/util"
+	"io/ioutil"
+	"fmt"
+	"regexp"
+	"strings"
+)
 
 func readStopWords(filename string) []string {
-	return nil
+	bytes, err := ioutil.ReadFile(filename)
+
+	if(err != nil) {
+		panic(fmt.Sprintf("Error while reading file: %s", filename))
+	}
+
+	return strings.Split(string(bytes), ",")
 }
 
+
+
 func readWords(filename string) []string {
-	return nil
+	bytes, err := ioutil.ReadFile(filename)
+
+	if(err != nil) {
+		panic(fmt.Sprintf("Error while reading file: %s", filename))
+	}
+
+	regex, err := regexp.Compile("[a-z]{2,}")
+
+	if(err != nil) {
+		panic("Error processing regex pattern.")
+	}
+
+	return regex.FindAllString(strings.ToLower(string(bytes)), -1)
 }
+
+
 
 func count(words []string, stopWords []string, wordFrequencies util.SortablePairList)  {
 
@@ -20,6 +48,7 @@ func printSorted(wordFrequencies util.SortablePairList)  {
 
 func main() {
 	stop_words := readStopWords("stop_words.txt")
+
 	words := readWords("input.txt")
 
 	wordFrequencyPairs := make(util.SortablePairList, 0)
