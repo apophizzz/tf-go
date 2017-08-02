f43721da9aea1ae1bac245c8a7893acbe05863af
Patrick.Kleindienst <patrick.kleindienst@web.de>
2017-08-02 21:55:40 +0200

    Proceeded with DataStorageManager

--- part4_object-interaction/11-letterbox/data_storage_manager.go
+++ part4_object-interaction/11-letterbox/data_storage_manager.go
@@ -1,10 +1,42 @@
 package storage
 
+import (
+	"fmt"
+)
+
 type DataStorageManager struct {
 }
 
 func (dsm *DataStorageManager) initialize(filePath string) {
 }
 
-func (dsm *DataStorageManager) dispatch(message []string) {
+func (dsm *DataStorageManager) Dispatch(message []string) error {
+	if len(message) >= 1 {
+		if message[0] == "init" {
+			if len(message) != 2 {
+				return &InvalidDataStorageManagerInitMessage{message}
+			} else {
+				return nil
+			}
+		} else {
+			return nil
+		}
+	} else {
+		return &EmptyDataStorageManagerMessage{}
+	}
+}
+
+type EmptyDataStorageManagerMessage struct {
+}
+
+func (err *EmptyDataStorageManagerMessage) Error() string {
+	return fmt.Sprintf("Cannot process empty message")
+}
+
+type InvalidDataStorageManagerInitMessage struct {
+	MalformedInitMessage []string
+}
+
+func (err *InvalidDataStorageManagerInitMessage) Error() string {
+	return fmt.Sprintf("Cannot process malformed init message: %s", err.MalformedInitMessage)
 }
--- part4_object-interaction/11-letterbox/data_storage_manager_test.go
+++ part4_object-interaction/11-letterbox/data_storage_manager_test.go
@@ -1,5 +1,6 @@
 package storage
 
+import "fmt"
 import "testing"
 
 func TestCanAllocateStruct(t *testing.T) {
@@ -25,10 +26,44 @@ func TestDataStorageManagerHasInitMethod(t *testing.T) {
 func TestDataStorageManagerHasDispatchMethod(t *testing.T) {
 	var iface interface{} = &DataStorageManager{}
 	_, ok := iface.(interface {
-		dispatch(message []string)
+		Dispatch(message []string) error
 	})
 
 	if !ok {
-		t.Error("Struct DataStorageManagaer does not have any method 'dispatch'!")
+		t.Error("Struct DataStorageManagaer does not have any method 'Dispatch'!")
+	}
+}
+
+func TestDispatchThrowsErrorOnEmptyMessage(t *testing.T) {
+	dataStorageManager := &DataStorageManager{}
+	message := []string{}
+	err := dataStorageManager.Dispatch(message)
+
+	if err == nil {
+		t.Error("Dispatch method should have returned an error!")
+	}
+
+	var actualErr interface{} = err
+	_, ok := actualErr.(*EmptyDataStorageManagerMessage)
+
+	if !ok {
+		t.Error(fmt.Sprintf("Dispatch method should have thrown EmptyDataStorageManagerMessage error, got %T", actualErr))
+	}
+}
+
+func TestDispatchThrowsErrorOnMalformedInitMessage(t *testing.T) {
+	dataStorageManager := &DataStorageManager{}
+	initMessage := []string{"init"}
+	err := dataStorageManager.Dispatch(initMessage)
+
+	if err == nil {
+		t.Error("Dispatch method should have returned an error!")
+	}
+
+	var expectedErr interface{} = err
+	_, ok := expectedErr.(*InvalidDataStorageManagerInitMessage)
+
+	if !ok {
+		t.Error("Dispatch method did not throw expected error type!")
 	}
 }
