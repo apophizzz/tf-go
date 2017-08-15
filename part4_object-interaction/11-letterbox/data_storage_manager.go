6d9c25bc0d90f2b0426bd981c46e31b9fc77ff43
Patrick.Kleindienst <patrick.kleindienst@web.de>
2017-08-15 20:47:41 +0200

    Refactored dispatch method

--- part4_object-interaction/11-letterbox/data_storage_manager.go
+++ part4_object-interaction/11-letterbox/data_storage_manager.go
@@ -20,10 +20,9 @@ func (dsm *DataStorageManager) Dispatch(message []string) error {
 				return inputErr
 			}
 
-			regexPattern := "[\\W_]+"
-			regex, _ := regexp.Compile(regexPattern)
-
-			dsm.fileContent = strings.ToLower(strings.TrimSpace(regex.ReplaceAllLiteralString(content, " ")))
+			dsm.fileContent = strings.ToLower(
+				cutTailingAndLeadingWhitespace(
+					replaceNonalnumCharsByWhitespace(content)))
 			return nil
 		}
 		return &InvalidDataStorageManagerMessage{message}
@@ -52,6 +51,16 @@ func readFileContent(filename string) (string, error) {
 		content = string(bytes)
 	}
 	return content, fileMissing
+}
+
+func replaceNonalnumCharsByWhitespace(fileContent string) string {
+	regexPattern := "[\\W_]+"
+	regex, _ := regexp.Compile(regexPattern)
+	return regex.ReplaceAllLiteralString(fileContent, " ")
+}
+
+func cutTailingAndLeadingWhitespace(fileContent string) string {
+	return strings.TrimSpace(fileContent)
 }
 
 type EmptyDataStorageManagerMessage struct {
